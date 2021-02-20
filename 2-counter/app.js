42dc4ae0ae3b76782f47bb6e7ed5c6229aa0cf6b
Asharaful Islam <Asharafulislam1@gmail.com>
2021-02-20 18:07:40 -0500

    The 2-counter has been modified by 3-madlibs

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -4,34 +4,53 @@ let count = 2;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+const companies = ["Uber", "Lyft", "Google", "Apple", "SpaceX", "Tinder"]
+const animals = ["Aardvark", "Blue Footed Booby", "Flying Dragon", "Giraffe Weevil", "Hammerhead Slug", "Komodo Dragon", "Naked Mole Rat", "Okapi", "Red Panda"]
+const vowels = ["A", "E", "I", "O", "U"]
+
 console.log(btns)
 
 // for (let i = 0; i < btns.length; i++) {
 //   // btn.addEventListener()
 // }
 
+function MadLibs() {
+  // These 4 lines of code are copied from 3-Madlibs/index.js lines 10-15
+  const companyName = companies[Math.floor(Math.random() * companies.length)]
+  const firstLetter = companyName.charAt(0)
+  const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!"
+  //outputDiv.innerHTML += companyString
+
+  return companyString //return the string instead of inserting it directly into html
+}
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
-      count--;
+      // Insert the companyString into the textContent of the button instead of the count number
+        //count--;
+      value.textContent = MadLibs()
     } else if (styles.contains("increase")) {
-      count *= 2;
-      // count = count * 2
+      // Insert the companyString into the textContent of the button instead of the count number
+        //count *= 2;
+      value.textContent = MadLibs()
     } else {
-      count *= 2;
+      // If the Reset button is pressed set the textContent to nothing.
+        //count = 1;
+      value.textContent = ""
     }
 
-    if (count > 0) {
-      value.style.color = "green";
-    }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.textContent = count;
+    // This code is not needed so I prevented it from running
+      // if (count > 0) {
+      //   value.style.color = "green";
+      // }
+      // if (count < 0) {
+      //   value.style.color = "red";
+      // }
+      // if (count === 0) {
+      //   value.style.color = "#222";
+      // }
+      // value.textContent = count;
   });
 });
